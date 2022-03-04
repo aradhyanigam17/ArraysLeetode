93edbc4500720ef284949b26fba57194ec6905e3
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-03-04 16:28:14 +0530

    Time: 4 ms (5.20%), Space: 42.9 MB (23.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -4,7 +4,25 @@ class Solution {
             nums1[m + i] = nums2[i];
         }
         
-        System.out.println(Arrays.toString(nums1)) ;
-        Arrays.sort(nums1) ;
+        // System.out.println(Arrays.toString(nums1)) ;
+        // Arrays.sort(nums1) ;
+        
+        for(int i = 0 ; i < nums1.length - 1 ;i++){
+            for(int j = i ; j >= 0 ; j--){
+                if(nums1[j] > nums1[j+1]){
+                    swap(nums1 , j , j + 1) ;
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    public void swap(int[] arr ,int i , int j){
+        int temp = arr[i] ;
+        arr[i] = arr[j];
+        arr[j] = temp ;
     }
 }
