9d35581a94e6dd8d487c7ee649e7cf56f118c38b
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-03-05 13:17:13 +0530

    Time: 1 ms (35.47%), Space: 43.1 MB (13.80%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -4,8 +4,10 @@ class Solution {
         int n = mat[0].length ;
         
         for(int i = 0 ; i < m ;i++){
-            for(int j = 0 ; j < n; j++){
-                if(mat[i][j] == target) return true; 
+            if(mat[i][0] <= target && mat[i][n-1] >= target){
+                for(int j = 0 ; j < n; j++){
+                    if(mat[i][j] == target) return true; 
+                }
             }
         }
         return false ;
