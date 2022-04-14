9da5386d44c65850101d75100ddf402e8edbf7ff
Aradhya Nigam <87924213+aradhyanigam17@users.noreply.github.com>
2022-04-14 16:24:28 +0530

    Time: 45 ms (23.63%), Space: 52 MB (86.40%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums) ;
-        return nums;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for(int i : nums){
+            minHeap.add(i);
+        }
+        
+        for(int i = 0 ; i < nums.length ;i++){
+         nums[i] = minHeap.poll();
+        }
+        
+        return nums ;
     }
+    
 }
